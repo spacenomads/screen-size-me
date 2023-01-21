29fe3f84b0a1f101a9f00ca1a7301001eac44dee
Carlos Mañas <carlos@spacenomads.com>
2023-01-21 10:28:42 +0100

    It works!!!

--- js/app.js
+++ js/app.js
@@ -1,30 +1,78 @@
 const app = document.querySelector('.js-app');
 const output = app.querySelector('.js-app-size');
 
+
+
 function getScreenHeight() {
   return window.screen.height;
 }
+
 
 function getScreenWidth() {
   return window.screen.width;
 }
 
+
 function getBrowserHeight() {
   return window.innerHeight;
 }
+
 
 function getBrowserWidth() {
   return window.innerWidth;
 }
 
-function getSize() {
-  return `<p><small>Pantalla:</small><br>${getScreenWidth()}x${getScreenHeight()} px</p>
-  <p><small>Navegador:</small><br>${getBrowserWidth()}x${getBrowserHeight()} px</p>`;
+function getSizes() {
+  return {
+    screen: {
+      width: getScreenWidth(),
+      height: getScreenHeight(),
+    },
+    browser: {
+      width: getBrowserWidth(),
+      height: getBrowserHeight(),
+    },
+  };
+}
+
+function getScreenSizeByOrientation(isLandscape, userScreen) {
+  return isLandscape & userScreen.width < userScreen.height
+    ?
+    `${userScreen.height}x${userScreen.width}`
+    :
+    `${userScreen.width}x${userScreen.height}`;
 }
 
 
+function getDeviceOrientation(isLandscape, userScreen) {
+  return isLandscape || !isLandscape & userScreen.width > userScreen.height ? 'Landscape' : 'Portrait';
+}
+
+
+function getSizeTemplate(orientation, screenSize, sizes) {
+  return `
+    <p class="app__size-block app__size-block--screen app__size-block--${orientation.toLowerCase()}">
+      <span class="app__size-icon"></span>
+      <span class="app__size-title">Screen (${orientation}):</span>
+      <span class="app__size-value">${screenSize} px</span>
+    </p>
+    <p class="app__size-block">
+      <span class="app__size-title">Browser:</span>
+      <span class="app__size-value">${sizes.browser.width}x${sizes.browser.height} px</span>
+    </p>`;
+}
+
+
+function getSize() {
+  const sizes = getSizes();
+  const isLandscape = window.matchMedia('(orientation: landscape)').matches;
+  const orientation = getDeviceOrientation(isLandscape, sizes.screen);
+  const screenSize = getScreenSizeByOrientation(isLandscape, sizes.screen);
+  return getSizeTemplate(orientation, screenSize, sizes);
+}
+
 function showResult() {
-  output.innerHTML =  getSize();
+  output.innerHTML = getSize();
 }
 
 window.addEventListener('resize', showResult);
